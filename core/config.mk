9c08648dc4f28e3f5650c0b529384c580606544a
RakeshBatra <raakesh.batra@rediffmail.com>
2020-11-06 03:13:09 -0800

    Add Sepolicy

--- core/config.mk
+++ core/config.mk
@@ -1248,9 +1248,13 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(COLT_BUILD),)
+ifneq ($(wildcard device/colt/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-# $(eval include vendor/reloaded/sepolicy/common/sepolicy.mk)
+$(eval include device/colt/sepolicy/common/sepolicy.mk)
+endif
+endif
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
